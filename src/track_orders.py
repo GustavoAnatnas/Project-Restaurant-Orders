e30ef218127592fe70149f95d8826499df5f388b
Gustavo Sant'Ana <98183666+GustavoAnatnas@users.noreply.github.com>
2023-03-21 00:59:50 +0000

    Análises contínuas/implementa classe TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,50 @@
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.orders = []
+        self.customers = {}
+        self.days = {}
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append((customer, order, day))
+        if customer not in self.customers:
+            self.customers[customer] = set()
+        self.customers[customer].add(order)
+        if customer not in self.days:
+            self.days[customer] = set()
+        self.days[customer].add(day)
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        if customer in self.customers:
+            order_count = {}
+            for order in self.customers[customer]:
+                order_count[order] = sum([1 for c, o, d in self.orders 
+                                          if c == customer and o == order])
+            if order_count:
+                return max(order_count, key=order_count.get)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        if customer in self.customers:
+            all_orders = set([o for c, o, d in self.orders])
+            return all_orders - self.customers[customer]
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        if customer in self.days:
+            all_days = set([d for c, o, d in self.orders])
+            return all_days - self.days[customer]
 
     def get_busiest_day(self):
-        pass
+        day_count = {}
+        for day in set([d for c, o, d in self.orders]):
+            day_count[day] = sum([1 for c, o, d in self.orders if d == day])
+        if day_count:
+            return max(day_count, key=day_count.get)
 
     def get_least_busy_day(self):
-        pass
+        day_count = {}
+        for day in set([d for c, o, d in self.orders]):
+            day_count[day] = sum([1 for c, o, d in self.orders if d == day])
+        if day_count:
+            return min(day_count, key=day_count.get)
